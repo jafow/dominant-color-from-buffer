29b9517450da249b8d980607356d7461b25319e9
jafow <jared.a.fowler@gmail.com>
2017-08-23 00:37:56 -0700

    Update example:
    Draw the image onto canvas first
    append it to dom so we can see something
    get the dominant from pixel buffer

--- example/index.js
+++ example/index.js
@@ -1,10 +1,33 @@
 function fetchImg(imgPath) {
   fetch(imgPath).then((res) => {
     console.log(`fetching ${imgPath}`)
-    return res.arrayBuffer()
+    return res.blob()
   }).then((buf) => {
-    let pixels = new Uint8ClampedArray(buf)
-    console.log(`pixel length: ${pixels.length}`)
-    console.log('pixels: ', pixels)
+    var canvas = createCanvas()
+    var ctx = canvas.getContext('2d')
+    var pic = URL.createObjectURL(buf)
+    var img = new Image()
+    img.src = pic
+    ctx.drawImage(img, 0, 0)
+    document.getElementById('app').appendChild(img)
+    var pix = ctx.getImageData(0, 0, 72, 72)
+    return getDominant(pix.data)
   })
+}
+
+function createCanvas () {
+  var canvas = document.createElement('canvas')
+  canvas.style.height = 100
+  canvas.style.width = 200
+  document.getElementById('app').appendChild(canvas)
+  return canvas
+}
+
+function writeIntoCtx (cvs, buf) {
+  var ctx = cvs.getContext('2d')
+  var imgData = new ImageData(buf, 72, 72)
+  var pix = ctx.createImageDatk(buf, 72, 72)
+}
+
+function getDominant () {
 }
